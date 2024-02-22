788e6e1d80ea21ef155cd084251e40026c6cc3da
Chan Lee <chanho0325@gmail.com>
2024-02-22 13:33:24 +0000

    Add Ch2. assignment results

---
+++ ch2/jiffies.c
@@ -0,0 +1,72 @@
+/**
+ * simple.c
+ *
+ * A simple kernel module.
+ *
+ * To compile, run makefile by entering "make"
+ *
+ * Operating System Concepts - 10th Edition
+ * Copyright John Wiley & Sons - 2018
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+#include <linux/jiffies.h>
+
+#define BUFFER_SIZE 128
+#define PROC_NAME "jiffies"
+
+ssize_t proc_read(struct file *file, char __user *usr_buf,
+        size_t count, loff_t *pos);
+
+static struct file_operations proc_ops = {
+    .owner = THIS_MODULE,
+    .read = proc_read,
+};
+
+/* This function is called when the module is loaded. */
+static int simple_init(void)
+{
+    proc_create(PROC_NAME, 0666, NULL, &proc_ops);
+
+    return 0;
+}
+
+/* This function is called when the module is removed. */
+static void simple_exit(void) {
+
+    remove_proc_entry(PROC_NAME, NULL);
+}
+
+ssize_t proc_read(struct file *file, char __user *usr_buf,
+        size_t count, loff_t *pos)
+{
+    int rv = 0;
+    char buffer[BUFFER_SIZE];
+    static int completed = 0;
+
+    if (completed) {
+        completed = 0;
+        return 0;
+    }
+
+    completed = 1;
+
+    rv = sprintf(buffer, "Jiffies: %lu\n", jiffies);
+
+    copy_to_user(usr_buf, buffer, rv);
+
+    return rv;
+}
+
+/* Macros for registering module entry and exit points. */
+module_init( simple_init );
+module_exit( simple_exit );
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Jiffies Module");
+MODULE_AUTHOR("SGG");
+
